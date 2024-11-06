313efb602c82f0dd557b7a242cb428e9643d24e3
Jaron420 <165937385+Jaron420@users.noreply.github.com>
2024-11-06 08:11:29 -0600

    Update index.js
    cleaning up

--- collaband_frontend/src/index.js
+++ collaband_frontend/src/index.js
@@ -1,12 +1,3 @@
-//import './index.css';
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>
-//);
-
 // src/index.js
 
 import React from 'react';
